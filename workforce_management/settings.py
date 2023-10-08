cc3dd1134d454c5ed20ff6edfcba01f725d09d4e
samyakgaur <samyakgaur@yahoo.com>
2023-10-08 11:53:30 +0530

    fix: enable api call for development

--- workforce_management/settings.py
+++ workforce_management/settings.py
@@ -72,7 +72,7 @@ CORS_ORIGIN_WHITELIST = [
      'https://new.expoundtechnivo.com',
  ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'workforce_management.urls'
