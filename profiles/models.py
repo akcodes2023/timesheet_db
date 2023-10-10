bcf9d3c10e2783e280c7fa75e9235ea59cabb37e
Akshay <akahaymishra181@gmail.com>
2023-10-10 10:31:05 +0530

    Profile and  User Commit

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ValidationError
 
 # Employee Event
 
 
 class Profile(models.Model):
     employee = models.AutoField(primary_key=True)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=20, null=False)
     first_name = models.CharField(max_length=20, null=False)
     last_name = models.CharField(max_length=20, null=False)
@@ -53,3 +56,21 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        # Check if this is a new Profile instance
+        if not hasattr(self, 'user'):
+            # Check if the username already exists
+            if User.objects.filter(username=self.username).exists():
+                raise ValidationError("Username already exists. Please choose a different username.")
+
+            # Create a User profile corresponding to the Profile instance
+            user = User(username=self.username)
+            # Update user fields as needed
+            user.first_name = self.first_name
+            user.last_name = self.last_name
+            user.email = self.email
+            user.save()
+
+        # Call the parent class's save method to save the Profile instance
+        super(Profile, self).save(*args, **kwargs)
